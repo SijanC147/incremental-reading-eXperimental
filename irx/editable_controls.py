f41ab364c9d49a3f2b6cd52b4642988d4b7b9116
Sean Bugeja <seanbugeja23@gmail.com>
2018-12-08 18:09:48 +0100

    reimplements the formatting approach from the ground up

--- irx/editable_controls.py
+++ irx/editable_controls.py
@@ -1,15 +1,15 @@
 REVIEWER_CONTROLS = {
-    "show help": "? i",
-    "toggle images": "Shift+m",
-    "toggle formatting": "Shift+f",
-    "toggle removed text": "Shift+z",
-    "toggle extracts": "Shift+x",
+    "show help": "?",
+    "toggle images": "Meta+m",
+    "toggle formatting": "Meta+f",
+    "toggle removed text": "Meta+z",
+    "toggle extracts": "Meta+x",
     "done (suspend)": "!",
     "undo": "u",
     "add image": "@",
     "add image (skip caption)": "Shift+@",
     "extract image": "m",
-    "extract image (skip caption)": "Meta+m",
+    "extract image (skip caption)": "q",
     "extract important": "1",
     "extract complimentary": "2",
     "extract important (and edit)": "Alt+1",
@@ -18,7 +18,7 @@ REVIEWER_CONTROLS = {
     "underline": "Ctrl+u",
     "italic": "Ctrl+i",
     "strikethrough": "Ctrl+s",
-    "remove": "z",
+    "remove": "Meta+d",
     "show reading list": "Ctrl+Alt+2",
     "show image manager": "i",
     "zoom in": "Ctrl+=",
@@ -36,4 +36,11 @@ IMAGE_MANAGER_CONTROLS = {
     "place image(s) above (for reordering)": "a",
     "place image(s) below (for reordering)": "b",
     "submit image changes": "Enter",
+}
+
+HIGHLIGHT_COLORS = {
+    "soon": ("#FFE11A", "#000000"),
+    "later": ("#FD7400", "#000000"),
+    "card": ("#1F8A70", "#000000"),
+    # "irx_extract": ("#FF971A", "#000000"),
 }
--- irx/web/model.js
+++ irx/web/model.js
@@ -1,203 +1,266 @@
-var imagesSidebar = "yes";
-var showFormatting = "yes";
-var showRemoved = "no";
-var showExtracts = "yes";
+var imagesSidebar = true;
+var stylesVisible = true;
+var highlightsVisible = true;
+var removedVisible = false;
 
-// function highlight(bgColor, textColor) {
-//     if (window.getSelection) {
-//         var range, sel = window.getSelection();
-
-//         if (sel.rangeCount && sel.getRangeAt) {
-//             range = sel.getRangeAt(0);
-//         }
-
-//         document.designMode = "on";
-//         if (range) {
-//             sel.removeAllRanges();
-//             sel.addRange(range);
-//         }
-
-//         document.execCommand("foreColor", false, textColor);
-//         document.execCommand("hiliteColor", false, bgColor);
-
-//         document.designMode = "off";
-//         sel.removeAllRanges();
-//     }
-// }
-
-// function markRange(identifier, bgColor, textColor) {
-//     var range, sel = window.getSelection();
-//     if (sel.rangeCount && sel.getRangeAt) {
-//         range = sel.getRangeAt(0);
-//         var startNode = document.createElement('span');
-//         startNode.setAttribute('id', ('sx' + identifier));
-//         startNode.setAttribute('irx-bg-color', bgColor);
-//         startNode.setAttribute('irx-text-color', textColor);
-//         range.insertNode(startNode);
-//         var endNode = document.createElement('span');
-//         endNode.setAttribute('id', ('ex' + identifier));
-//         range.collapse(false);
-//         range.insertNode(endNode);
-//         range.setStartAfter(startNode);
-//         range.setEndBefore(endNode);
-//         sel.removeAllRanges();
-//         sel.addRange(range);
-//     }
-// }
-
-// function selectMarkedRange(identifier) {
-//     var startNode, endNode, range, sel;
-//     startNode = document.getElementById('sx' + identifier);
-//     endNode = document.getElementById('ex' + identifier);
-//     if (startNode && endNode) {
-//         range = document.createRange();
-//         range.setStartAfter(startNode);
-//         range.setEndBefore(endNode);
-//         sel = window.getSelection();
-//         sel.removeAllRanges();
-//         sel.addRange(range);
-//     }
-// }
-
-// function restoreHighlighting() {
-//     var startNodesXPathResult = document.evaluate('//*[@ir-bg-color]', document, null, XPathResult.ANY_TYPE, null);
-//     var sNodes = new Array();
-//     var startNode = startNodesXPathResult.iterateNext();
-//     while (startNode) {
-//         sNodes.push(startNode);
-//         startNode = startNodesXPathResult.iterateNext();
-//     }
-//     var id;
-//     for (var i = 0; i < sNodes.length; i++) {
-//         startNode = sNodes[i];
-//         id = startNode.id.substring(1);
-//         selectMarkedRange(id);
-//         highlight(startNode.getAttribute('irx-bg-color'),
-//             startNode.getAttribute('irx-text-color'))
-//     }
-// }
-
-// function turnoffHighlighting() {
-//     var startNodesXPathResult = document.evaluate('//*[@irx-bg-color]', document, null, XPathResult.ANY_TYPE, null);
-//     var sNodes = new Array();
-//     var startNode = startNodesXPathResult.iterateNext();
-//     while (startNode) {
-//         sNodes.push(startNode);
-//         startNode = startNodesXPathResult.iterateNext();
-//     }
-//     var id;
-//     for (var i = 0; i < sNodes.length; i++) {
-//         startNode = sNodes[i];
-//         id = startNode.id.substring(1);
-//         selectMarkedRange(id);
-//         highlight('transparent', 'black')
-//     }
-// }
-
-function removeText() {
-    var selection = window.getSelection().getRangeAt(0);
-    var selectedText = selection.extractContents();
-    var span = document.createElement("span");
-
-    span.className = "irx-removed";
-    span.setAttribute("irx-show-removed", displayRemoved);
-    span.appendChild(selectedText);
-
-    selection.insertNode(span);
+function markRange(identifier, attributes) {
+    var range, sel = window.getSelection();
+    if (sel.type == "Range") {
+        range = sel.getRangeAt(0);
+        var startNode = document.createElement('span');
+        startNode.setAttribute('id', ('sx' + identifier));
+        for (var attr in attributes) {
+            if (attributes.hasOwnProperty(attr)) {
+                if ((attr == "styles") & startNode.hasAttribute("irx-styles")) {
+                    attrVal = startNode.getAttribute("irx-styles") + " " + attributes[attr];
+                    attrVals = attrVal.split(" ");
+                    attrVal = attrVals.filter(function (v, i, self) { return (self.indexOf(v) === i & v.length > 0) });
+                    attrVal = attrVal.join(" ");
+                } else {
+                    attrVal = attributes[attr];
+                }
+                startNode.setAttribute('irx-' + attr, attrVal);
+            }
+        }
+        range.insertNode(startNode);
+        var endNode = document.createElement('span');
+        endNode.setAttribute('id', ('ex' + identifier));
+        range.collapse(false);
+        range.insertNode(endNode);
+        range.setStartAfter(startNode);
+        range.setEndBefore(endNode);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
 }
 
-function linkToNote(note_id, extract_type) {
-    var selection = window.getSelection().getRangeAt(0);
-    var selectedText = selection.extractContents();
-
-    var note_link = document.createElement("a");
-    note_link.className = "irx-link"
-    note_link.setAttribute("href", "irxnid:" + note_id)
-    note_link.appendChild(selectedText);
-
-    var note_span = document.createElement("span");
-    var extract_span_class = "irx-extract-link-" + extract_type
-    note_span.className = "irx-extract " + extract_span_class;
-    note_span.setAttribute("irx-show-extracts", showExtracts);
-    note_span.appendChild(note_link)
-
-    selection.insertNode(note_span);
+function selectMarkedRange(identifier) {
+    var startNode, endNode, range, sel;
+    startNode = document.getElementById('sx' + identifier);
+    endNode = document.getElementById('ex' + identifier);
+    if (startNode && endNode) {
+        range = document.createRange();
+        range.setStartAfter(startNode);
+        range.setEndBefore(endNode);
+        sel = window.getSelection();
+        sel.removeAllRanges();
+        sel.addRange(range);
+        return startNode;
+    }
+    return false;
 }
 
-function format(style) {
-    var selection = window.getSelection().getRangeAt(0);
-    var selectedText = selection.extractContents();
-    var span = document.createElement("span");
+function execCommandOnRange(identifiers, attrs, clear) {
+    if (!Array.isArray(identifiers) & !selectMarkedRange(identifiers)) {
+        identifier = identifiers;
+        markRange(identifier, attrs);
+        identifiers = Array();
+        identifiers.push(identifier);
+    }
+    for (var i in identifiers) {
+        identifier = identifiers[i]
+        var startNode = selectMarkedRange(identifier);
+        if (startNode) {
+            var range, sel = window.getSelection();
 
-    span.className = "irx-format " + style;
-    span.setAttribute("irx-show-formatting", showFormatting);
-    span.appendChild(selectedText);
+            if (sel.rangeCount && sel.getRangeAt) {
+                range = sel.getRangeAt(0);
+            }
 
-    selection.insertNode(span);
+            document.designMode = "on";
+            if (range) {
+                sel.removeAllRanges();
+                sel.addRange(range);
+            }
+
+            if (clear) {
+                document.execCommand("removeFormat", false, null);
+            }
+
+            if (startNode.hasAttribute("irx-remove")) {
+                if (removedVisible) {
+                    document.execCommand("hiliteColor", false, "grey");
+                    document.execCommand("foreColor", false, "white");
+                } else {
+                    document.execCommand("hiliteColor", false, "black");
+                    document.execCommand("foreColor", false, "white");
+                }
+            } else {
+                if (startNode.hasAttribute("irx-link")) {
+                    var irx_link = startNode.getAttribute("irx-link");
+                    document.execCommand("createLink", false, "irxnid:" + irx_link);
+                }
+                if (startNode.hasAttribute("irx-styles") & stylesVisible) {
+                    var irx_styles = startNode.getAttribute("irx-styles").split(" ");
+                    for (var s in irx_styles) {
+                        document.execCommand(irx_styles[s], true, true);
+                    }
+                }
+                if (startNode.hasAttribute("irx-bg") & highlightsVisible) {
+                    var irx_bg = startNode.getAttribute("irx-bg");
+                    document.execCommand("hiliteColor", false, irx_bg);
+                }
+                if (startNode.hasAttribute("irx-fg") & highlightsVisible) {
+                    var irx_fg = startNode.getAttribute("irx-fg");
+                    document.execCommand("foreColor", false, irx_fg);
+                }
+            }
+            document.designMode = "off";
+            sel.removeAllRanges();
+        }
+    }
+}
+
+function rangeIdsWithAttr(irx_attr) {
+    var xPathQuery = '//*[@' + irx_attr + ']';
+    var xPathResult = document.evaluate(xPathQuery, document, null, XPathResult.ANY_TYPE, null);
+    var node_ids = [];
+    while (node = xPathResult.iterateNext()) {
+        node_ids.push(node.id.substring(2));
+    }
+    return node_ids;
+}
+
+function toggleStyles(manual) {
+    prev_state = stylesVisible;
+    if (!manual | manual == "toggle") {
+        stylesVisible = !stylesVisible;
+    } else {
+        stylesVisible = manual;
+    }
+    if (prev_state != stylesVisible) {
+        execCommandOnRange(rangeIdsWithAttr('irx-styles'), null, true);
+    }
+}
+
+function toggleHighlights(manual) {
+    prev_state = highlightsVisible;
+    if (!manual | manual == "toggle") {
+        highlightsVisible = !highlightsVisible;
+    } else {
+        highlightsVisible = manual;
+    }
+    if (prev_state != highlightsVisible) {
+        execCommandOnRange(rangeIdsWithAttr('irx-bg'), null, true);
+    }
+}
+
+function toggleRemoved(manual) {
+    prev_state = removedVisible;
+    if (!manual | manual == "toggle") {
+        removedVisible = !removedVisible;
+    } else {
+        removedVisible = manual;
+    }
+    if (prev_state != removedVisible) {
+        execCommandOnRange(rangeIdsWithAttr('irx-remove'), null, true);
+    }
 }
 
 function toggleImagesSidebar(manual) {
-    var text = document.getElementsByClassName("irx-text")[0];
-    var images = document.getElementsByClassName("irx-images")[0];
-    if (manual == "toggle") {
-        if (imagesSidebar == "no") {
-            imagesSidebar = "yes";
-        } else {
-            imagesSidebar = "no";
-        }
+    var prev_state = imagesSidebar;
+    if (!manual | manual == "toggle") {
+        imagesSidebar = !imagesSidebar;
     } else {
-        imagesSidebar = manual
+        imagesSidebar = manual;
     }
-    text.setAttribute("irx-images-sidebar", imagesSidebar)
-    images.setAttribute("irx-images-sidebar", imagesSidebar)
-}
-
-
-function toggleShowExtracts(manual) {
-    if (manual == "toggle") {
-        if (showExtracts == "no") {
-            showExtracts = "yes";
-        } else {
-            showExtracts = "no";
-        }
-    } else {
-        showExtracts = manual
-    }
-    var elems = document.getElementsByClassName("irx-extract");
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].setAttribute("irx-show-extracts", showExtracts)
+    if (prev_state != imagesSidebar) {
+        var attrVal = imagesSidebar ? "yes" : "no";
+        var text = document.getElementsByClassName("irx-text")[0];
+        var images = document.getElementsByClassName("irx-images")[0];
+        text.setAttribute("irx-images-sidebar", attrVal);
+        images.setAttribute("irx-images-sidebar", attrVal);
     }
 }
 
+// function removeText() {
+//     var selection = window.getSelection().getRangeAt(0);
+//     var selectedText = selection.extractContents();
+//     var span = document.createElement("span");
 
-function toggleShowRemoved(manual) {
-    if (manual == "toggle") {
-        if (showRemoved == "no") {
-            showRemoved = "yes";
-        } else {
-            showRemoved = "no";
-        }
-    } else {
-        showRemoved = manual
-    }
-    var elems = document.getElementsByClassName("irx-removed");
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].setAttribute("irx-show-removed", showRemoved)
-    }
-}
+//     span.className = "irx-removed";
+//     span.setAttribute("irx-show-removed", displayRemoved);
+//     span.appendChild(selectedText);
 
-function toggleShowFormatting(manual) {
-    if (manual == "toggle") {
-        if (showFormatting == "yes") {
-            showFormatting = "no";
-        } else {
-            showFormatting = "yes";
-        }
-    } else {
-        showFormatting = manual
-    }
-    var elems = document.getElementsByClassName("irx-format");
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].setAttribute("irx-show-formatting", showFormatting)
-    }
-}
+//     selection.insertNode(span);
+// }
+
+// function linkToNote(note_id, extract_type) {
+//     var selection = window.getSelection().getRangeAt(0);
+//     var selectedText = selection.extractContents();
+
+//     var note_link = document.createElement("a");
+//     note_link.className = "irx-link"
+//     note_link.setAttribute("href", "irxnid:" + note_id)
+//     note_link.appendChild(selectedText);
+
+//     var note_span = document.createElement("span");
+//     var extract_span_class = "irx-extract-link-" + extract_type
+//     note_span.className = "irx-extract " + extract_span_class;
+//     note_span.setAttribute("irx-show-extracts", showExtracts);
+//     note_span.appendChild(note_link)
+
+//     selection.insertNode(note_span);
+// }
+
+// function format(style) {
+//     var selection = window.getSelection().getRangeAt(0);
+//     var selectedText = selection.extractContents();
+//     var span = document.createElement("span");
+
+//     span.className = "irx-format " + style;
+//     span.setAttribute("irx-show-formatting", showFormatting);
+//     span.appendChild(selectedText);
+
+//     selection.insertNode(span);
+// }
+
+
+// function toggleShowExtracts(manual) {
+//     if (manual == "toggle") {
+//         if (showExtracts == "no") {
+//             showExtracts = "yes";
+//         } else {
+//             showExtracts = "no";
+//         }
+//     } else {
+//         showExtracts = manual
+//     }
+//     var elems = document.getElementsByClassName("irx-extract");
+//     for (var i = 0; i < elems.length; i++) {
+//         elems[i].setAttribute("irx-show-extracts", showExtracts)
+//     }
+// }
+
+
+// function toggleShowRemoved(manual) {
+//     if (manual == "toggle") {
+//         if (showRemoved == "no") {
+//             showRemoved = "yes";
+//         } else {
+//             showRemoved = "no";
+//         }
+//     } else {
+//         showRemoved = manual
+//     }
+//     var elems = document.getElementsByClassName("irx-removed");
+//     for (var i = 0; i < elems.length; i++) {
+//         elems[i].setAttribute("irx-show-removed", showRemoved)
+//     }
+// }
+
+// function toggleShowFormatting(manual) {
+//     if (manual == "toggle") {
+//         if (showFormatting == "yes") {
+//             showFormatting = "no";
+//         } else {
+//             showFormatting = "yes";
+//         }
+//     } else {
+//         showFormatting = manual
+//     }
+//     var elems = document.getElementsByClassName("irx-format");
+//     for (var i = 0; i < elems.length; i++) {
+//         elems[i].setAttribute("irx-show-formatting", showFormatting)
+//     }
+// }
