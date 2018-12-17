75c051d608031ef5e33cec3270539cb8bd00a780
Sean Bugeja <seanbugeja23@gmail.com>
2018-12-17 20:41:25 +0100

    adds some explainer text to the editable controls file

--- irx/editable_controls.py
+++ irx/editable_controls.py
@@ -4,6 +4,21 @@
 #   CTRL -> CMD
 #   ALT -> OPT
 
+#   Always use the WIN version of the keys when defining controls here,
+#   the information above is provided just as a guideline.
+#   Once in Anki, IR3X should display the correct key-sequences based
+#   on the platform it is running on.
+#
+#   Multiple key sequences can be assigned to the same action by separating them
+#   using a space eg.
+#
+#    ...
+#       "show help": "? Meta+h i", // assigns three key shortcuts to the action.
+#    ...
+#
+#   IR3X will notify you of any conflicts that it finds on load, and should bring up
+#   the help menu to highlight which controls are in conflict.
+
 REVIEWER_CONTROLS = {
     "show help": "?",
     "toggle images": "Meta+m",
