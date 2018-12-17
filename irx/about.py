3188a1737a0b08245e8f10cc9a43e91da36af52e
Sean Bugeja <seanbugeja23@gmail.com>
2018-12-17 14:12:44 +0100

    updates the about section

--- irx/about.py
+++ irx/about.py
@@ -8,7 +8,6 @@ from PyQt4.QtCore import Qt
 from aqt import mw
 
 from irx._version import __version__
-from irx.util import db_log
 
 IR_ORIGINAL_GITHUB_URL = 'https://github.com/luoliyan/incremental-reading'
 IRX_GITHUB_URL = "https://github.com/SijanC147/incremental-reading-eXperimental"
@@ -18,7 +17,8 @@ def showAbout():
     dialog = QDialog(mw)
 
     label = QLabel()
-    text = '''
+    label.setText(
+        '''
 <div style="text-align: center">
 <p style="font-weight: bold; font-size: 16px;">Incremental Reading 3 eXperimental</p>
 <p> v3.8.3x-beta </p>
@@ -28,17 +28,18 @@ def showAbout():
 <p>Everything in this add-on is inspired by the incredible work of </p>
 <p>Joseph Lorimer</p>
 <p>Tiago Barroso, Frank Kmiec, Aleksej, Christian Weiß, Timothée Chauvin</p>
-<p>Who actively maintain the <a href="{1}">original IR add-on</a> this builds on.</p>
+<p>Who actively maintain the original <a href="{1}">IR add-on</a>, which is the foundation of this add-on.</p>
+<p><u>Everything</u> implemented in this add-on was only made possible through reverse-engineering the original to learn Qt.</p>
 <hr/>
 <p>The motivation behind this add-on was initially trying to port some of the v4 features to the</p>
-<p>ANKI2 compatible v3 version, for those who, like myself, aren't ready to let go of some ANKI2 addons.</p>
+<p>Anki 2.0 compatible v3 version, for those who, like myself, aren't ready to make the move to Anki 2.1.</p>
 <p>Consider everything in this add-on <i>experimental and barely functional</i>, this was a learning experience</p> 
 <p>more than anything, however I will try my best to maintain and build on it moving forward.</p>
-<p>I will be taking a break from it for some time following this release due to other obligations, but I figured maybe</p>
-<p>if some people try it out I could get some feedback and have issues ready to fix when I pick it up again.</p>
-<p>Any and all input is greatly appreciated.</p>
+<p>I will be taking a break from it for some time following this release (mid-December 2018) due to my academic obligations.</p>
+<p>However, I figured maybe if some people try it out I could get some feedback and have issues ready to fix when I pick it up again.</p>
+<p>Any and all input is greatly appreciated. Thank you for trying this add-on.</p>
 </div>'''.format(IRX_GITHUB_URL, IR_ORIGINAL_GITHUB_URL)
-    label.setText(text)
+    )
     label.setAlignment(Qt.AlignCenter)
     label.setStyleSheet(
         """
@@ -49,12 +50,12 @@ def showAbout():
     )
 
     label.setOpenExternalLinks(True)
-    buttonBox = QDialogButtonBox(QDialogButtonBox.Ok)
-    buttonBox.accepted.connect(dialog.accept)
+    button_box = QDialogButtonBox(QDialogButtonBox.Ok)
+    button_box.accepted.connect(dialog.accept)
 
     layout = QVBoxLayout()
     layout.addWidget(label)
-    layout.addWidget(buttonBox)
+    layout.addWidget(button_box)
 
     dialog.setLayout(layout)
     dialog.setWindowTitle('About IR3X')
